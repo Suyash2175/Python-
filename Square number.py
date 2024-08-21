04175dc54f1b09241f564892021f7aa24e80b913
Suyash2175 <127495941+Suyash2175@users.noreply.github.com>
2024-08-21 22:24:13 +0530

    Create Square number.py

---
+++ Square number.py
@@ -0,0 +1,12 @@
+# Function to square a given number
+def square_number(n):
+    return n * n
+
+# Creating a list of the first 50 integers
+integers_list = list(range(1, 51))
+
+# Example usage: Squaring each number in the list
+squared_numbers = [square_number(n) for n in integers_list]
+
+# Printing the squared numbers
+print(squared_numbers)
